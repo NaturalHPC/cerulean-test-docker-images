53747714f0cc13ab9d16a229590444928123c5d8
Lourens Veen <l.veen@esciencecenter.nl>
2018-10-20 12:01:30 +0200

    Add a flaky container that randomly drops SSH connections

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -11,9 +11,13 @@ docker build -t cerulean-test-torque-6 .
 cd $BASEDIR
 
 cd cerulean_test_docker_images/container-slurm
-docker build -t cerulean-test-slurm-14-11 -f slurm-14-11.Dockerfile .
-docker build -t cerulean-test-slurm-15-08 -f slurm-15-08.Dockerfile .
-docker build -t cerulean-test-slurm-16-05 -f slurm-16-05.Dockerfile .
-docker build -t cerulean-test-slurm-17-02 -f slurm-17-02.Dockerfile .
-docker build -t cerulean-test-slurm-17-11 -f slurm-17-11.Dockerfile .
+docker build -t mdstudio/cerulean-test-slurm-14-11 -f slurm-14-11.Dockerfile .
+docker build -t mdstudio/cerulean-test-slurm-15-08 -f slurm-15-08.Dockerfile .
+docker build -t mdstudio/cerulean-test-slurm-16-05 -f slurm-16-05.Dockerfile .
+docker build -t mdstudio/cerulean-test-slurm-17-02 -f slurm-17-02.Dockerfile .
+docker build -t mdstudio/cerulean-test-slurm-17-11 -f slurm-17-11.Dockerfile .
+cd $BASEDIR
+
+cd cerulean_test_docker_images/container-flaky
+docker build -t mdstudio/cerulean-test-flaky .
 cd $BASEDIR
