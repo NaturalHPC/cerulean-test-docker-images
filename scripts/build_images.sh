3abb8c307722c0562841b8e17fc8a6e9f937a2f1
Lourens Veen <l.veen@esciencecenter.nl>
2018-12-06 14:04:37 +0100

    Add Slurm 18.08

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -16,6 +16,7 @@ docker build -t mdstudio/cerulean-test-slurm-15-08 -f slurm-15-08.Dockerfile .
 docker build -t mdstudio/cerulean-test-slurm-16-05 -f slurm-16-05.Dockerfile .
 docker build -t mdstudio/cerulean-test-slurm-17-02 -f slurm-17-02.Dockerfile .
 docker build -t mdstudio/cerulean-test-slurm-17-11 -f slurm-17-11.Dockerfile .
+docker build -t mdstudio/cerulean-test-slurm-18-08 -f slurm-18-08.Dockerfile .
 cd $BASEDIR
 
 cd cerulean_test_docker_images/container-flaky
