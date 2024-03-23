fadab7f394e910f19a7944710810a976b4824318
Lourens Veen <l.veen@esciencecenter.nl>
2024-03-23 14:04:45 +0100

    Switch to Docker buildx to build

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -5,33 +5,33 @@ set -e      # exit on error
 BASEDIR="$PWD"
 
 cd cerulean_test_docker_images/container-base-old
-docker build -t naturalhpc/cerulean-test-base-old .
+docker buildx build -t naturalhpc/cerulean-test-base-old .
 cd $BASEDIR
 
 cd cerulean_test_docker_images/container-base
-docker build -t naturalhpc/cerulean-test-base .
+docker buildx build -t naturalhpc/cerulean-test-base .
 cd $BASEDIR
 
 cd cerulean_test_docker_images/container-webdav
-docker build -t naturalhpc/cerulean-test-webdav .
+docker buildx build -t naturalhpc/cerulean-test-webdav .
 cd $BASEDIR
 
 cd cerulean_test_docker_images/container-torque
-docker build -t naturalhpc/cerulean-test-torque-6 .
+docker buildx build -t naturalhpc/cerulean-test-torque-6 .
 cd $BASEDIR
 
 cd cerulean_test_docker_images/container-slurm
-docker build -t naturalhpc/cerulean-test-slurm-base-old -f slurm-base-old.Dockerfile .
-docker build -t naturalhpc/cerulean-test-slurm-base -f slurm-base.Dockerfile .
-docker build -t naturalhpc/cerulean-test-slurm-16-05 -f slurm-16-05.Dockerfile .
-docker build -t naturalhpc/cerulean-test-slurm-17-02 -f slurm-17-02.Dockerfile .
-docker build -t naturalhpc/cerulean-test-slurm-17-11 -f slurm-17-11.Dockerfile .
-docker build -t naturalhpc/cerulean-test-slurm-18-08 -f slurm-18-08.Dockerfile .
-docker build -t naturalhpc/cerulean-test-slurm-19-05 -f slurm-19-05.Dockerfile .
-docker build -t naturalhpc/cerulean-test-slurm-20-02 -f slurm-20-02.Dockerfile .
-docker build -t naturalhpc/cerulean-test-slurm-20-11 -f slurm-20-11.Dockerfile .
+docker buildx build -t naturalhpc/cerulean-test-slurm-base-old -f slurm-base-old.Dockerfile .
+docker buildx build -t naturalhpc/cerulean-test-slurm-base -f slurm-base.Dockerfile .
+docker buildx build -t naturalhpc/cerulean-test-slurm-16-05 -f slurm-16-05.Dockerfile .
+docker buildx build -t naturalhpc/cerulean-test-slurm-17-02 -f slurm-17-02.Dockerfile .
+docker buildx build -t naturalhpc/cerulean-test-slurm-17-11 -f slurm-17-11.Dockerfile .
+docker buildx build -t naturalhpc/cerulean-test-slurm-18-08 -f slurm-18-08.Dockerfile .
+docker buildx build -t naturalhpc/cerulean-test-slurm-19-05 -f slurm-19-05.Dockerfile .
+docker buildx build -t naturalhpc/cerulean-test-slurm-20-02 -f slurm-20-02.Dockerfile .
+docker buildx build -t naturalhpc/cerulean-test-slurm-20-11 -f slurm-20-11.Dockerfile .
 cd $BASEDIR
 
 cd cerulean_test_docker_images/container-flaky
-docker build -t naturalhpc/cerulean-test-flaky .
+docker buildx build -t naturalhpc/cerulean-test-flaky .
 cd $BASEDIR
